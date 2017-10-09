39504c31828a5b5325e5ab1c18870f9c5268e70b
Shunki Takami <tkmnet@users.noreply.github.com>
2017-10-09 16:02:32 +0900

    Update: rrscluster

---
+++ script/initserver_pass.sh
@@ -0,0 +1,32 @@
+#!/bin/bash
+
+. ./config.cfg
+
+cd `dirname $0`
+
+SUDOPW=$1
+
+PKEY=`cat ~/.ssh/id_rsa.pub`
+
+function serverProc {
+#    ssh $1 sh -c "\"echo '${PKEY}' >> ~/.ssh/authorized_keys\""
+#    ssh $1 sh -c "'cat ~/.ssh/authorized_keys | uniq | tee ~/.ssh/authorized_keys'"
+    echo "$SUDOPW" | ssh -tt $1 sudo 'sh -c "mkdir -p /var/tmp/robocup ; chmod 777 /var/tmp/robocup"' | sed 1,2d
+    ssh $1 mkdir -p /var/tmp/robocup/$2
+    scp -r ../roborescue ${1}:/var/tmp/robocup/$2/roborescue
+}
+
+function clientProc {
+#    ssh $1 sh -c "\"echo '${PKEY}' >> ~/.ssh/authorized_keys\""
+#    ssh $1 sh -c "'cat ~/.ssh/authorized_keys | uniq | tee ~/.ssh/authorized_keys'"
+    echo "$SUDOPW" | ssh -tt $1 sudo 'sh -c "mkdir -p /var/tmp/robocup ; chmod 777 /var/tmp/robocup"' | sed 1,2d
+    ssh $1 mkdir -p /var/tmp/robocup/$2
+}
+
+clientProc $SERVER_C1 F
+clientProc $SERVER_C2 P
+clientProc $SERVER_C3 A
+serverProc $SERVER_SS S
+
+SUDOPW=""
+
