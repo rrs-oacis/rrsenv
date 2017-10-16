873f24c0e79b9622a3e898c4935281aa75807d6f
Shunki Takami <tkmnet@users.noreply.github.com>
2017-10-16 13:17:09 +0900

    Update

--- script/startNG_MT.sh
+++ script/startNG_MT.sh
@@ -13,17 +13,21 @@ function serverProc {
     ssh $1 sh -c "'export LC_ALL=en_US.UTF-8; export DISPLAY=:0; export AWT_FORCE_HEADFUL=true; cd /var/tmp/robocup/${2}/roborescue/boot; ./start-comprun.sh -m ../../MAP/${MAP}/map -c ../../MAP/${MAP}/config -t ${AAGENT}'" >${OACIS_WORKDIR}/server.log
     sleep 3
 
+    EXTENSION="tar.gz"
     if [ $ARCHIVER = "7zip" ]; then
         ssh $1 sh -c "'cd /var/tmp/robocup/${2}/roborescue/boot; 7za a -m0=lzma2 ../../LOG/${DATECODE}-${MAP}/${DATECODE}-${MAP}-${AAGENT}.7z logs'"
+       EXTENSION="7z"
     else
         if [ $ARCHIVER = "zip" ]; then
             ssh $1 sh -c "'cd /var/tmp/robocup/${2}/roborescue/boot; zip -r ../../LOG/${DATECODE}-${MAP}/${DATECODE}-${MAP}-${AAGENT}.zip logs'"
+            EXTENSION="zip"
         else
             ssh $1 sh -c "'cd /var/tmp/robocup/${2}/roborescue/boot; tar zcf ../../LOG/${DATECODE}-${MAP}/${DATECODE}-${MAP}-${AAGENT}.tar.gz logs'"
         fi
     fi
 
-    scp $1:"/var/tmp/robocup/${2}/LOG/${DATECODE}-${MAP}/${DATECODE}-${MAP}-${AAGENT}.7z" ${OACIS_WORKDIR}/simulation_log.7z
+    scp $1:"/var/tmp/robocup/${2}/LOG/${DATECODE}-${MAP}/${DATECODE}-${MAP}-${AAGENT}.${EXTENSION}" ${OACIS_WORKDIR}/simulation_log.${EXTENSION}
+
     ssh $1 sh -c "'cd /var/tmp/robocup/${2}/roborescue/boot; echo "${DATECODE},${MAP},${FAGENT},${PAGENT},${AAGENT},"; sh ./print-lastscore.sh'" | tee ${OACIS_WORKDIR}/score.txt
     ssh $1 sh -c "'export LC_ALL=en_US.UTF-8; export DISPLAY=:0; rm -rf /tmp/img_log; mkdir /tmp/img_log; cd /var/tmp/robocup/${2}/roborescue/boot; bash ./logextract.sh ./logs/rescue.log /tmp/img_log'"
     scp -r $1:/tmp/img_log ${OACIS_WORKDIR}/
